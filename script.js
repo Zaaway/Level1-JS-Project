cb93b89e5e3979b7e3f3523a7c4ebbe7590b0e03
Zaaway <zhaowei2543996411@gmail.com>
2023-10-04 15:31:39 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const form = document.getElementById("myForm");
+form.addEventListener("submit", checkGuess);
+
+function generateCode(){
+    const code = Math.floor((Math.random()*999)+1);
+}
+
+function checkGuess(){
+    
+}
