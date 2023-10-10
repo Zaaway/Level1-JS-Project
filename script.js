528e3a329fd379617fd13722ae4480eda0bc12e8
Zaaway <zhaowei2543996411@gmail.com>
2023-10-09 22:51:09 -0400

    asdf

--- script.js
+++ script.js
@@ -1,10 +1,55 @@
 const form = document.getElementById("myForm");
-form.addEventListener("submit", checkGuess);
+let guessInput;
+let code;
+let count = 7;
+let gameOver = false;
+
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    if(gameOver){
+        return;
+    }
+
+    guessInput = document.getElementById("guess").value;
+    if(!guessInput){
+        document.getElementById("response").innerHTML="Make a guess first.";
+        return;
+    }
+
+    count--;
+
+    if(count==0){
+        endGame();
+    }
+
+    checkGuess();
+});
 
 function generateCode(){
-    const code = Math.floor((Math.random()*999)+1);
+    code = Math.floor((Math.random()*999)+1);
 }
 
 function checkGuess(){
-    
+    if(gameOver){
+        document.getElementById("guessCount").innerHTML=`You have 0 guesses left`;
+        return;
+    }
+
+    if(guessInput==code){
+        document.getElementById("response").innerHTML="Correct!";
+    }
+    else if(guessInput<code){
+        document.getElementById("response").innerHTML="Try guessing higher";
+    }
+    else{
+        document.getElementById("response").innerHTML="Try guessing lower";
+    }
+
+    document.getElementById("guessCount").innerHTML=`You have ${count} guesses left`;
+}
+
+function endGame(){
+    gameOver = true;
+    document.getElementById("response").innerHTML="Uh oh! You did not crack the code in time and the police have arrived. Maybe you'll have better luck in your next life.";
 }
